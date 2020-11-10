794c7c8327d04e35938684268fedf4d84372a799
GopalDas123 <Gopaldax@gmail.com>
2020-11-10 15:50:40 +0500

    Tinny Animation Project Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="app">
+      <h1>Project-4B (Animations) By <a href="https://gopaldas.surge.sh">
+        
+        Gopal
         </a>
-      </header>
+        </h1>
+      <div className="card" data-aos="fade-down"><h2>Animation 1</h2></div>
+      <div className="card" data-aos="fade-left"><h2>Animation 2</h2></div>
+      <div className="card" data-aos="fade-right"><h2>Animation 3</h2></div>
+      <div className="card" data-aos="fade-up-right"><h2>Animation 4</h2></div>
+      <div className="card" data-aos="zoom-out"><h2>Animation 5</h2></div>
+      <div className="card" data-aos="flip-down"><h2>Animation 6</h2></div>
+      <div className="card" data-aos="fade-down"><h2>Animation 7</h2></div>
+      <div className="card" data-aos="zoom-out"><h2>Animation 8</h2></div>
+      <div className="card" data-aos="zoom-in"><h2>Animation 9</h2></div>
+      <div className="card" data-aos="flip-left"><h2>Animation 10</h2></div>
+      {/* <div className="card" data-aos="flip-left">Animation 10</div> */}
+      
     </div>
   );
 }
